51848f8eaa77f1353f886cfa4d1e8ea5e6e3ac7b
Patrick Rudolph <patrick.rudolph@9elements.com>
2022-03-17 11:28:21 +0100

    MctpCoreLib: Add unit tests using cmocka
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

---
+++ MdePkg/Library/MctpCoreLib/main.c
@@ -0,0 +1,112 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <inttypes.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <Base.h>
+#include <Uefi.h>
+#include <Library/MctpTransportLib.h>
+#include <Library/MctpCoreLib.h>
+
+static EFI_STATUS TransportSendMessageRetval;
+static BOOLEAN TransportSendMessageCalled;
+static MCTP_TRANSPORT_HEADER TransportSendHdr;
+uint8_t *TransportSendMessageBodyHeader;
+size_t TransportSendMessageBodyHeaderLength;
+uint8_t *TransportSendMessageBody;
+size_t TransportSendMessageBodyLength;
+
+/* STUB of linked libraries */
+EFI_STATUS
+MctpTransportSendMessage(
+  IN MCTP_TRANSPORT_HEADER *Hdr,
+  IN UINT8                 *MessageBodyHeader,
+  IN UINTN                 MessageBodyHeaderLength,
+  IN UINT8                 *MessageBody,
+  IN UINTN                  MessageBodyLength,
+  IN UINTN                 TimeoutUsec
+  )
+{
+	memcpy(&TransportSendHdr, Hdr, sizeof(TransportSendHdr));
+	TransportSendMessageBodyHeader = malloc(MessageBodyHeaderLength);
+	if (!TransportSendMessageBodyHeader) {
+		return EFI_OUT_OF_RESOURCES;
+	}
+	memcpy(TransportSendMessageBodyHeader, MessageBodyHeader, MessageBodyHeaderLength);
+	TransportSendMessageBodyHeaderLength = MessageBodyHeaderLength;
+
+	TransportSendMessageBody = malloc(MessageBodyLength);
+	if (!TransportSendMessageBody) {
+		return EFI_OUT_OF_RESOURCES;
+	}
+	memcpy(TransportSendMessageBody, MessageBody, MessageBodyLength);
+	TransportSendMessageBodyLength = MessageBodyLength;
+
+	TransportSendMessageCalled = TRUE;
+	return TransportSendMessageRetval;
+}
+
+EFI_STATUS
+EFIAPI
+MctpTransportReceiveMessage(
+  OUT  MCTP_MSG              **Msg,
+  OUT  UINTN                 *MsgLength,
+  IN   UINTN                 TimeoutUsec
+  )
+{
+	return EFI_SUCCESS;
+}
+
+
+UINT8
+EFIAPI
+MctpPhysicalGetEndpointInformation(
+  VOID
+  )
+{
+  return 0;
+}
+
+/* UNIT TESTS */
+
+/* A test case that does nothing and succeeds. */
+static void test_MctpCoreRegisterMessageClass(void **state) {
+	EFI_STATUS Status;
+	UINT8 tests[] = {
+		MCTP_TYPE_CONTROL_MSG,
+		MCTP_TYPE_PLDM_MSG,
+		MCTP_TYPE_NCSI_MSG,
+		MCTP_TYPE_ETHERNET_MSG,
+		MCTP_TYPE_NVME_MSG,
+		MCTP_TYPE_SPDM_MSG,
+		MCTP_TYPE_PCI_VENDOR,
+		MCTP_TYPE_IANA_VENDOR,
+	};
+
+	for (size_t i = 0; i < ARRAY_SIZE(tests); i++) {
+		Status = MctpCoreRegisterMessageClass(tests[i]);
+		assert_int_equal(Status, EFI_SUCCESS);
+	}
+	Status = MctpCoreRegisterMessageClass(MCTP_TYPE_IANA_VENDOR + 1);
+	assert_int_equal(Status, EFI_INVALID_PARAMETER);
+}
+
+extern EFI_STATUS
+EFIAPI
+MctpCoreLibConstructor (
+  VOID
+  );
+
+int main(void) {
+	MctpCoreLibConstructor();
+
+	TransportSendMessageRetval = EFI_SUCCESS;
+
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_MctpCoreRegisterMessageClass),
+	};
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
