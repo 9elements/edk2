b4a8828742ff2f17c5da09c08a24c80c96122cef
Lean Sheng Tan <sheng.tan@9elements.com>
2022-06-14 15:46:09 +0200

    UefiPayloadPkg: Boot menu configuration enabling
    Enable EEPROM to disable the boot menu.
    
    Signed-off-by: Lean Sheng Tan <sheng.tan@9elements.com>

--- MdeModulePkg/Universal/BdsDxe/BdsEntry.c
+++ MdeModulePkg/Universal/BdsDxe/BdsEntry.c
@@ -1201,7 +1201,9 @@ BdsEntry (
     // Execute Key####
     //
     PERF_INMODULE_BEGIN ("BdsWait");
-    BdsWait (HotkeyTriggered);
+    if (PcdGet16 (PcdPlatformBootTimeOut) != 0) {
+      BdsWait (HotkeyTriggered);
+    }
     PERF_INMODULE_END ("BdsWait");
     //
     // BdsReadKeys() can be removed after all keyboard drivers invoke callback in timer callback.
