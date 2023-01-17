0c9d708eace4eeb61578fe3b3bbc06fc23565b8c
Milo-D <David.Milosevic@web.de>
2023-01-17 16:17:53 +0100

    Creating setupmenu components in the VFR instead of in the runtime.

--- UefiPayloadPkg/Library/SetupMenuUiLib/SetupMenuNVDataStruc.h
+++ UefiPayloadPkg/Library/SetupMenuUiLib/SetupMenuNVDataStruc.h
@@ -4,22 +4,106 @@
 #pragma pack(1)
 typedef struct {
 
-  UINT8 Hyperthreading;
-  UINT8 TurboMode;
-  UINT8 Cx;
-  UINT8 CxLimit;
-  UINT8 PrimaryDisplay;
-  UINT8 EnergyEfficientTurbo;
-  UINT8 LlcDeadline;
-  UINT8 IntelVtx;
-  UINT8 IntelVtd;
-  UINT8 SecureBoot;
-  UINT8 PxeRetries;
-  UINT8 PwrG3;
-  UINT8 PcieSsc;
-  UINT8 PcieSris;
-  UINT8 Ibecc;
-    
-} SETUPMENU_CONFIGURATION;
+  UINT8 Value;
+
+} STORAGE_HYPERTHREADING;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_TURBOMODE;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_CX;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_CX_LIMIT;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_PRIMARY_DISPLAY;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_EE_TURBO;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_LLC_DEADLINE;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_INTEL_VTX;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_INTEL_VTD;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_SECURE_BOOT;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_PXE_RETRIES;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_PWR_G3;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_PCIE_SSC;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_PCIE_SRIS;
+
+#pragma pack(1)
+typedef struct {
+
+  UINT8 Value;
+
+} STORAGE_IBECC;
 
 #endif
