72baad4629ba11118c0c5deeb570ff5abd3bc0d6
Patrick Rudolph <patrick.rudolph@9elements.com>
2025-01-30 14:41:08 +0100

    PcatRealTimeClockRuntimeDxe: Preserve VRT bit in StatusD
    On recent AMD platforms the VRT bit in the StatusD register is
    read-writeable. Adjust the code to not clear it as it would
    RtcWaitToUpdate() to return an error.
    
    On platforms where the VRT bit is read-only the introduced code
    does the same as before.
    
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- PcAtChipsetPkg/PcatRealTimeClockRuntimeDxe/PcRtc.c
+++ PcAtChipsetPkg/PcatRealTimeClockRuntimeDxe/PcRtc.c
@@ -303,9 +303,12 @@ PcRtcInit (
   RtcRead (RTC_ADDRESS_REGISTER_C);
 
   //
-  // Clear RTC register D
+  // Clear RTC register D with changing the VRT bit.
+  // On most platforms VRT is read-only, but when it's not it must
+  // be preserved to prevent failing in RtcWaitToUpdate().
   //
-  RegisterD.Data = FixedPcdGet8 (PcdInitialValueRtcRegisterD);
+  RegisterD.Data = RtcRead (RTC_ADDRESS_REGISTER_D);
+  RegisterD.Data = FixedPcdGet8 (PcdInitialValueRtcRegisterD) | (RegisterD.Data & BIT7);
   RtcWrite (RTC_ADDRESS_REGISTER_D, RegisterD.Data);
 
   //
